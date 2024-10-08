1df44397e63129883c98b47e590d075f36e1125f
JuanGarcia <82130952+ChilyGarcia@users.noreply.github.com>
2024-10-08 17:41:16 -0500

    [ FIX ]: Fix problems with deploy

--- src/app/(modern)/page.tsx
+++
@@ -1,6 +0,0 @@
-import ModernScreen from '@/components/screens/modern-screen';
-import SwapPageModern from './swap/page';
-
-export default function IndexPageModern() {
-  return <SwapPageModern />;
-}
